265ff888d3305ccd08780c071f3c5f5341305423
chernikovanv <52738292+chernikovanv@users.noreply.github.com>
2021-05-02 12:31:31 +0300

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,11 +1,13 @@
-import os
 from flask import Flask
+from flask_restplus import Resource, Api
 
-app = Flask(__name__)
+app = Flask(__name__)                  #  Create a Flask WSGI application
+api = Api(app)                         #  Create a Flask-RESTPlus API
 
-@app.route('/')
-def start():
-    return('ok')
+@api.route('/hello')                   #  Create a URL route to this resource
+class HelloWorld(Resource):            #  Create a RESTful resource
+    def get(self):                     #  Create GET endpoint
+        return {'hello': 'world'}
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.getenv('PORT'))
