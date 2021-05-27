4a2fe8a6ff741bbb78394b25e68b61039b800068
Anton <chernikovanv@gmail.com>
2021-05-27 21:50:48 +0300

    update

--- api/api.py
+++ api/api.py
@@ -9,8 +9,8 @@ import werkzeug
 werkzeug.cached_property = werkzeug.utils.cached_property
 
 # monkey patch - https://stackoverflow.com/questions/67496857/cannot-import-name-endpoint-from-view-func-from-flask-helpers-in-python
-#import flask.scaffold
-#flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
+import flask.scaffold
+flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
 
 from flask_restplus import Resource, Api
 from flask_restplus import reqparse
